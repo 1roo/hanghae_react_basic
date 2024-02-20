419631521936e96edbcb601a19fd35a67d009b6a
hanaru <133945249+1roo@users.noreply.github.com>
2024-02-20 20:05:31 +0900

    JSX(JsvaScript + XML)

--- react/week-1/src/App.js
+++ react/week-1/src/App.js
@@ -1,46 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-function Child() {
-  return (
-  <>
-    <div>자식 컴포넌트</div>
-    <GrandChild />
-  </>
-  )
-}
+export default function App() {
+  const number = 1;
 
-  function GrandChild() {
-  return <div>손자 컴포넌트</div>;
-}
-
-
-function App() {
-  // <---- 자바스크립트 영역 ---->
-  const handleClick = () => {
-    alert("클릭!");
+  const pTagStyle = {
+    color: "blue",
   }
 
   return (
-    /* <---- HTML/JSX 영역  ---->*/
-    <>
-      <Child />
-      <div
+    <div className='test-class'>
+      <p style={pTagStyle}>안녕하세요 리액트입니다.</p>
+      {/* 삼항연산자 */}
+      <p
         style={{
-          height: '100vh',
-          display: ' flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
+          color: "red",
         }}
       >
-        {/* 이곳에 퀴즈를 위한 html 코드를 작성해 주세요 */}
-        <span>이것은 내가 만든 App컴포넌트 입니다.</span>
-        <button type='button' onClick={handleClick}>클릭!</button>
-      </div>
-    </>
-  );
-
-
+        {number > 10 ? number + '은 10보다 크다' : number + '은 10보다 작다.'}</p>
+    </div>
+  )
 }
-
-export default App;
