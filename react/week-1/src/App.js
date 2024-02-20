990be079180165a17f9ce515754a095f4cc53477
hanaru <133945249+1roo@users.noreply.github.com>
2024-02-20 22:55:20 +0900

    state-1

--- react/week-1/src/App.js
+++ react/week-1/src/App.js
@@ -1,9 +1,52 @@
-import React from 'react';
-import Child from 'Child';
+import { useState } from 'react';
+import "./App.css";
 
 function App() {
-  const name = 'test';
-  return <Child name={name}>이름</Child>
-}
+  /*
+    //hook
+    // const [state, setState] = useState('initial Value');
+    const [count, setCount] = useState(0);
+    const [todoList, setTodoList] = useState([]);
+  
+    //useState, useEffect, useContext, useMemo
+    //useDispatch, useSelect...
+  
+    return (
+      <div>STATE에 대한 이야기입니다.</div>
+    )
+  }
+  */
 
-export default App
+  /*
+  const [name, setName] = useState('김할아버지');
+
+  return (
+    <div>
+      {name}
+      <br />
+      <button onClick={function() {
+        setName('박할머니');
+      }}>클릭</button>
+    </div>
+  );
+}
+*/
+
+  const [fruit, setFruit] = useState("");
+
+  return (
+    <div>
+      과일: <input
+        value={fruit}
+        onChange={function (event) {
+          console.log('event: ', event.target.value);
+          setFruit(event.target.value);
+        }}
+      />
+      <br /><br />
+      {fruit}
+    </div>
+  )
+
+}
+export default App;
