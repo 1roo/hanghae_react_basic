8f4fc0bb9a289d929c5b80abca92f7b7a5bdf0c3
hanaru <133945249+1roo@users.noreply.github.com>
2024-02-21 14:08:27 +0900

    반복 컴포넌트1

--- react/style-project/src/App.js
+++ react/style-project/src/App.js
@@ -18,15 +18,35 @@ function App() {
   //   alignItems: "center",
   // }
 
+  const testArr = ['감자', '고구마', '오이', '가지', '옥수수']
 
   return (
-    <div className = "app-style">
-      <div className = "component-style">감자</div>
-      <div className = "component-style">고구마</div>
-      <div className = "component-style">오이</div>
-      <div className = "component-style">가지</div>
-      <div className = "component-style">옥수수</div>
+    <div className="app-style">
+      {/* {testArr.map(function (item) {
+        return <div className="component-style">{item}</div>
+      })} */}
+
+      {/* {testArr.map((item) => {
+        return <div className="component-style">{item}</div>
+      })} */}
+
+      {testArr.filter(function (item) {
+        return item !== "오이";
+      })
+      .map(function (item) {
+        return <div className="component-style">{item}</div>
+      })}
     </div>
+
+
+
+    // <div className = "app-style">
+    //   <div className = "component-style">감자</div>
+    //   <div className = "component-style">고구마</div>
+    //   <div className = "component-style">오이</div>
+    //   <div className = "component-style">가지</div>
+    //   <div className = "component-style">옥수수</div>
+    // </div>
   );
 }
 
