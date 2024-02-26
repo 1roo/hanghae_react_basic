68dc2b33adba68ed1a9fd55dfe0264d8b4fc71ff
hanaru <133945249+1roo@users.noreply.github.com>
2024-02-26 14:04:52 +0900

    숙련 -2 전역 스타일링, reset.css

--- react/week-2/styledcomponents/src/App.js
+++ react/week-2/styledcomponents/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import styled from 'styled-components';
+import TestPage from './components/TestPage';
+import GlobalStyle from './GlobalStyle';
 
 const StContainer = styled.div`
   display: flex;
@@ -17,7 +19,7 @@ const boxList = ['red', 'blue', 'green', 'yellow'];
 
 //색을 넣으면 이름을 반환
 const getBoxName = (color) => {
-  switch(color) {
+  switch (color) {
     case 'red':
       return '빨간박스';
     case 'blue':
@@ -34,20 +36,26 @@ const getBoxName = (color) => {
 
 function App() {
   return (
-    <StContainer>
-      {
-        boxList.map((box)=> {
-          return <StBox borderColor={box}>{getBoxName(box)}</StBox>
-        })
-      }
+    // <StContainer>
+    //   {
+    //     boxList.map((box)=> {
+    //       return <StBox borderColor={box}>{getBoxName(box)}</StBox>
+    //     })
+    //   }
 
-      {/* <StBox borderColor='red'>박스</StBox>
-      <StBox borderColor='blue'>박스</StBox>
-      <StBox borderColor='green'>박스</StBox>
-      <StBox borderColor='yellow'>박스</StBox> */}
-    </StContainer>
-
+    //   {/* <StBox borderColor='red'>박스</StBox>
+    //   <StBox borderColor='blue'>박스</StBox>
+    //   <StBox borderColor='green'>박스</StBox>
+    //   <StBox borderColor='yellow'>박스</StBox> */}
+    // </StContainer>
+    <>
+      <GlobalStyle />
+      <TestPage
+        title='제목입니다'
+        contents='내용입니다' />
+    </>
   );
+
 }
 
 export default App;
